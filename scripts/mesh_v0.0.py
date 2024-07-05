807b1fcd4652d27f6c0043dee0a6ffef8a19afd5
tomsail <saillour.thomas@gmail.com>
2024-07-05 11:39:57 +0200

    feat: added result files export

--- scripts/mesh_v0.0.py
+++ scripts/mesh_v0.0.py
@@ -1,10 +1,15 @@
-import pyposeidon.model as pm
+import os
+import glob
 import geopandas as gp
 import pandas as pd
+import numpy as np
+import searvey
 import logging
 import xarray as xr
 from pyposeidon.telemac import flip
-import numpy as np
+from pyposeidon.utils import data
+import pyposeidon.model as pm
+import pyposeidon.meteo as pmeteo
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -46,7 +51,48 @@ def fix_mesh(b):
     )
 
 
-def main(mesh: bool = True, model: bool = True):
+def get_meta() -> gp.GeoDataFrame:
+    meta_web = searvey.get_ioc_stations().drop(columns=["lon", "lat"])
+    meta_api = (
+        pd.read_json(
+            "http://www.ioc-sealevelmonitoring.org/service.php?query=stationlist&showall=all"
+        )
+        .drop_duplicates()
+        .drop(columns=["lon", "lat"])
+        .rename(columns={"Code": "ioc_code", "Lon": "lon", "Lat": "lat"})
+    )
+    merged = pd.merge(
+        meta_web,
+        meta_api[["ioc_code", "lon", "lat"]].drop_duplicates(),
+        on=["ioc_code"],
+    )
+    updated = merged.assign(
+        geometry=gp.points_from_xy(merged.lon, merged.lat, crs="EPSG:4326")
+    )
+    return updated
+
+
+def ioc_subset_from_files_in_folder(
+    df: pd.DataFrame, folder: str, ext: str = ".parquet"
+):
+    """this function return a subset of the ioc database from all the files (json or parquet)
+    present in a folder
+    """
+    list_files = []
+    for file in os.listdir(folder):
+        name = file.split(ext)[0]
+        if file.endswith(ext):
+            list_files.append(name)
+    return df[df.ioc_code.isin(list_files)]
+
+
+YEAR = 2023
+PROJECT = ""
+WIND = glob.glob(PROJECT + f"02_meteo/era5/lon_lat/netcdf/{YEAR}*.nc")
+WIND += glob.glob(PROJECT + f"02_meteo/era5/lon_lat/netcdf/{YEAR+1}*.nc")
+
+
+def main(mesh: bool = True, model: bool = True, results=True):
     #
     # general meshing settings (oceanmesh settings below)
     #
@@ -54,18 +100,19 @@ def main(mesh: bool = True, model: bool = True):
     res_min = 0.5
     res_max = res_min * 10
     cbuffer = res_min / 10
+    # obs
+    obs_path = "v0.0/ioc_.csv"
+    ioc_ = get_meta()
+    ioc_cleanup = ioc_subset_from_files_in_folder(ioc_, "../ioc_cleanup/clean/")
+    ioc_cleanup.to_csv(obs_path)
     # Folders and files for mesh generation
-    seaset_path = "v0.0/seaset_full.csv"
-    seaset_full = pd.read_csv(
-        "https://raw.githubusercontent.com/tomsail/seaset/2176a61197d136121878a5412f39e3351d646af5/Notebooks/catalog_full.csv",
-        index_col=0,
-    ).to_csv(seaset_path)
     gshhs_folder = "../coastlines/out/"
     fdem = "00_bathy/etopo/ETOPO_0.03.nc"
     coasts = gp.read_parquet(gshhs_folder + f"gshhg_{resolution}_nosea.parquet")
     #
     rpath = "v0.0"
-    model = {
+
+    MODEL = {
         # "type": "tri2d",
         "coastlines": coasts,
         "mesh_generator": "oceanmesh",
@@ -86,8 +133,9 @@ def main(mesh: bool = True, model: bool = True):
         # model
         "solver_name": "telemac",
         "start_date": "2023-7-1 0:0:0",
-        "end_date": "2023-7-1 23:0:0",
+        "end_date": "2023-7-14 23:0:0",
         "meteo_input360": True,  # if meteo files longitudes go from from 0 to 360
+        "meteo_source": None,  # path to meteo files
         "monitor": True,  # get time series for observation points
         # "obs": seaset_path,
         "update": ["dem"],
@@ -106,7 +154,7 @@ def main(mesh: bool = True, model: bool = True):
     # first step --- create mesh
     mesh_file = rpath + f"/GSHHS_{resolution}_{res_min}.gr3"
     if mesh:
-        b = pm.set(**model)
+        b = pm.set(**MODEL)
         b.create()
         b.mesh.to_file(mesh_file)
 
@@ -114,19 +162,35 @@ def main(mesh: bool = True, model: bool = True):
     if model:
         for solver in ["schism", "telemac"]:
             rpath = "v0.0/" + solver
-            model["mesh_file"] = mesh_file
-            model["rpath"] = rpath
-            model["solver_name"] = solver
-            model["coastlines"] = None  # skip this step, we don't need coastlines
-            model["obs"] = seaset_path  # apply obs only to export "station.in" file
-            model["update"] = ["model"]
-            b = pm.set(**model)
+            MODEL["mesh_file"] = mesh_file
+            MODEL["rpath"] = rpath
+            MODEL["solver_name"] = solver
+            MODEL["coastlines"] = None  # skip this step, we don't need coastlines
+            MODEL["obs"] = obs_path  # apply obs only to export "station.in" file
+            MODEL["update"] = ["model"]
+            meteo = pmeteo.Meteo(WIND)
+            meteo.Dataset = meteo.Dataset.sel(
+                time=slice(MODEL["start_date"], MODEL["end_date"])
+            )
+            MODEL["meteo_source"] = meteo
+            b = pm.set(**MODEL)
             b.create()
             b.output()
             fix_mesh(b)
             b.mesh.to_file(f"v0.0/{solver}/hgrid.gr3")
             b.save()
             b.set_obs()
+            b.run()
+
+    # third step --- extract results
+    if results:
+        for solver in ["telemac"]:  # schism not tested
+            MODEL["rpath"] = "v0.0/" + solver
+            MODEL["solver_name"] = solver
+            MODEL["result_type"] = "2D"
+            MODEL["convert_results"] = True
+            MODEL["extract_TS"] = True
+            res = data.get_output(**MODEL)
 
 
 if __name__ == "__main__":
